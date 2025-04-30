a8d74840cdb2ecbb21ec9765162cb10db8ba8c65
daihaoxin <daihaoxin@126.com>
2025-04-30 23:23:57 +0800

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+// 导入global.css文件，将全局样式添加到您的应用程序中：
+// 您可以使用此文件将CSS规则添加到应用程序中的所有路由
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
